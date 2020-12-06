80420b4a81629ac3ac426a8130c19ef5a3ef1ac2
Youness BENNAJ <youness.bennaj@gmail.com>
2020-12-06 21:55:57 +0100

    use mongoose and Joi to make some data validation on user registration

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const { ApolloServer } = require('apollo-server');
-const { connect, Schema, model, Types } = require("mongoose");
+const { ApolloServer, AuthenticationError } = require('apollo-server');
+//Package pour hasher le mdp de l'utilisateur
+
+const bcrypt = require('bcrypt');
+
+// *** MONGODB *** 
+const { connect, Schema, model, Types, set } = require("mongoose");
 //On importe l'URI de notre base de donnée depuis le fichier de config
 //On ajoute ce fichier à .gitignore pour ne pas le pusher sur le repo publique
 const { MONGODB } = require("./config");
+
+// *** MODELS *** //
+
 //On importe nos model pour créer ou récupérer nos instances en DB
-const { User } = require("./models/User");
+const { User, validateUser } = require("./models/User");
 const { Post } = require("./models/Post");
+
+// *** TYPEDEFS ***
+
 //On importe nos typeDefs
 const { typeDefs } = require("./graphql/typeDefs");
 
@@ -43,15 +54,71 @@ const resolvers = {
         },
     },
     Mutation: {
-        register: (_, { registerInput: { password } }) => {
-            console.log(password);
-            return "User registration !!"
+        register: (_, { registerInput: { username, email, password, confirmPassword } }) => {
+            const { error } = validateUser({ username, email, password, confirmPassword });
+            //Si le model de donnée de l'utilisateur n'est pas validé, on envoi une erreur d'autentification
+            if (error) throw new AuthenticationError(error.details[0].message);
+
+            //On va ensute vérifier que l'utilisateur n'existe pas en DB 
+            User.findOne({ email })
+                .then(doc => {
+                    //Si l'utilisateur est déjà présent en DB...
+                    if (doc) {
+                        throw new AuthenticationError("User already registred.");
+                    } else {
+                        //Sinon on peut passer à la création d'un nouvel utilisateur en DB... 
+
+                        //On va d'abord hasher le mot de passe pour le stocker en DB de manière sécurisé
+                        bcrypt.genSalt(10, function (err, salt) {
+                            bcrypt.hash(password, salt, function (err, hash) {
+
+                                let user = new User({
+                                    username,
+                                    email,
+                                    password: hash,
+                                    createdAt: new Date().toISOString()
+                                })
+
+                                console.log(user);
+
+                                // user.save().then(doc => {
+                                //     return doc;
+                                // }).catch(err => {
+                                //     console.log(err);
+                                // });
+                            });
+                        });
+                        // let user = new User({
+                        //     username,
+                        //     email,
+                        //     password,
+                        //     confirmPassword,
+                        //     createdAt
+                        // })
+
+                        // User.create(user).then(user => {
+                        //     console.log(user);
+                        // });
+
+                    }
+                })
+            // User.create({ username })
+            //     .then(user => console.log(user))
+            //     .catch(err => console.error(err.message))
+            //On va hasher notre mot de passe 
+            // bcrypt.genSalt(10, function (err, salt) {
+            //     bcrypt.hash(password, salt, function (err, hash) {
+            //         console.log(hash);
+            //     });
+            // });
+
         }
     }
 }
 
 //On va utiliser mongoose, qui sert d'interface de programmation entre notre server et mongoDB
 //On va se connecter à notre base de donnée via l'URI fourni par MongoDB Atlas
+set('useCreateIndex', true);
 connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
--- models/User.js
+++ models/User.js
@@ -1,15 +1,67 @@
 const { Schema, model } = require("mongoose");
+//Utilisation de Joi pour valider nos données 
+const Joi = require('joi');
 
 //On va définir la "shape" de notre document représentant un user en DB
 const userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String,
-    createdAt: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    createdAt: {
+        type: String,
+        required: true
+    }
 })
 
+//On va valider les données de notre utilisateur
+const validateUser = (user) => {
+    const schema = Joi.object({
+        username: Joi.string()
+            .alphanum()
+            .min(5)
+            .max(50)
+            .required(),
+
+        email: Joi.string()
+            .email()
+            .min(5)
+            .max(255)
+            .required(),
+
+        password: Joi.string()
+            .min(4)
+            .max(1024)
+            .required(),
+
+        confirmPassword: Joi.any().equal(Joi.ref('password'))
+            .required()
+            .label('Confirm password')
+            .messages({ 'any.only': '{{#label}} does not match' }),
+
+    })
+
+    return schema.validate(user);
+}
+
 //On va compiler notre schema en model => Le model est le blueprint à partir duquel on va créer nos objet en DB
-//User = classe; user = instance 
+//User = classe; user = instance
 const User = model("User", userSchema);
 
 //E.G
@@ -20,4 +72,4 @@ const User = model("User", userSchema);
 //     createdAt: '2020-12-06T13:27:29'
 // })
 
-module.exports = { User }
+module.exports = { User, validateUser, userSchema };
